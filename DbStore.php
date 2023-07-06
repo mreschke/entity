11cd8ebd9e3638dc78ba6687c8650aa7c01bf94c
Matthew Reschke <mail@mreschke.com>
2023-07-06 11:25:10 -0600

    Revert DbStore save optimizations, doesnt work when we do pass in IDs for record that dont exist, like syncing client_feeds, dealers, etc... we want to SET the IDs but they dont exist in db yet.

--- DbStore.php
+++ DbStore.php
@@ -444,7 +444,8 @@ abstract class DbStore extends Store implements StoreInterface
         $increments = $this->attributes('increments');
 
         if (is_array($entities)) {
-            // Insert BULK records, assumg the do NOT already exist in DB
+
+            // Save bulk records
             $records = [];
             foreach ($entities as $entity) {
                 // If $primary is null, remove it so auto-increment will work in PostgreSQL
@@ -464,41 +465,26 @@ abstract class DbStore extends Store implements StoreInterface
             $this->fireEvent('created', $entities);
 
         } else {
-            // Insert or Update a SINGLE record
 
-            // Table uses a primary key
-            $found = false;
+            // If $primary is null, remove it so auto-increment will work in PostgreSQL
             if (isset($primary)) {
-
-                // If $primary is null, remove it so auto-increment will work in PostgreSQL
                 if (!isset($entities->$primary) || ($increments && $entities->$primary == 0)) {
                     unset($entities->$primary);
                 }
+            }
 
-                // Entity has an existing PK value already, determine if we should isnert or update
+            if (isset($primary)) {
+                $found = false;
                 if (isset($entities->$primary)) {
-
                     // Smart insert or update based on primary key selection
                     $handle = $this->table()->where($primaryColumn, $entities->$primary);
 
-                    if ($increments) {
-                        // If autoIncrement=True AND entity already has a PK value,
-                        // then assume its an UPDATE because the record must have
-                        // already been inserted or how would we know the PK?
-                        $found = true;
-                    } else {
-                        // Table has no autoIncrement, so our PK is probably set
-                        // manually with a human made id/key.  Therefore we need
-                        // to QUERY the existing data to determine if already
-                        // exists, to perform an insert vs update.
-                        // Check if record exists by executing TOP 1 on it (->first())
-                        // This actually executes a 'select id from table'
-                        $found = !is_null($handle->select($primaryColumn)->first());
+                    // Check if record exists by executing TOP 1 on it (->first())
+                    $found = !is_null($handle->select($primaryColumn)->first());
 
-                        // Remove the limit 1 added from the ->first() above, for some reason it sticks
-                        // And causes an UPDATE xyz LIMIT 1 statement, which is usually fine, but errors on some MySQL platforms (PlanetScale)
-                        $handle->limit = null;
-                    }
+                    // Remove the limit 1 added from the ->first() above, for some reason it sticks
+                    // And causes an UPDATE xyz LIMIT 1 statement, which is usually fine, but errors on some MySQL platforms (PlanetScale)
+                    $handle->limit = null;
                 }
 
                 // Updating an existing record
